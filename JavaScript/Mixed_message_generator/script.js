218c0fe49e426930584539459c4d16479bcb89f4
Bishal Baral <iamlegendbishal@gmail.com>
2024-02-07 10:55:52 +0545

    added jokes and poem

--- JavaScript/Mixed_message_generator/script.js
+++ JavaScript/Mixed_message_generator/script.js
@@ -8,6 +8,18 @@ const dat = {
     "London Bridge is falling down, Falling down, falling down.",
     "Mary had a little lamb, Its fleece was white as snow.",
   ],
+  _joke: [
+    "What do you call fake spaghetti? An impasta.",
+    "Why couldn't the bicycle stand up by itself? It was two-tired.",
+    "What do you call cheese that isn't yours? Nacho cheese.",
+    "Why don't scientists trust atoms? Because they make up everything.",
+  ],
+  _poem: [
+    "A man is a man, a man is a man.",
+    "In the quiet of the night, Underneath the stars so bright, Whispers float upon the air, Telling tales of love and care.",
+    "Nature's lullaby gently sings, As the moon its light it brings, Dreams dance in the silent space, where worries vanish without trace",
+    "Roses are red, violets are blue, Sugar is sweet, and so are you.",
+  ],
 };
 
-console.log(dat._song)
+console.log(dat._song);
