4182a8a52bc3cd1155b6e718bec1685981a47bc1
Bishal Baral <iamlegendbishal@gmail.com>
2024-02-07 10:48:23 +0545

    drastic changes

---
+++ JavaScript/Mixed_message_generator/script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to mood message generator");
+const dat = {
+  _mood: ["happy", "sad", "angry"],
+  _activity: ["joke", "poem", "song"],
+  _song: [
+    "Twinkle, twinkle, little star, How I wonder what you are.",
+    "Row, row, row your boat, Gently down the stream.",
+    "London Bridge is falling down, Falling down, falling down.",
+    "Mary had a little lamb, Its fleece was white as snow.",
+  ],
+};
+
+console.log(dat._song)
